4cad421921d3050b61ed51c8aa34a991283e37dd
harufumi.kamioka <kamioka.harufumi@gmail.com>
2017-12-12 11:27:23 +0900

    SecondKadaiAppの作成 AutoLayOutの設定

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func unwind(_ segue: UIStoryboardSegue){
+    }
+
+    @IBOutlet weak var textArea: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +24,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // segueから遷移先のResultViewControllerを取得する
+        let resultViewController : ResultViewController = segue.destination as! ResultViewController
+        resultViewController.name = textArea.text!
+    }
 
-
+   
 }
-
